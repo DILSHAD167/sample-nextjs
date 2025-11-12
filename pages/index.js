ddcadf328ae2cae7699bbdfddfe6a8a8a097554f
dilshad <dilshad167@gmail.com>
2025-11-12 09:02:56 +0000

    new commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>Hello World from Next.js!</h1>
+      <p>Deployed via Jenkins Pipeline 🚀</p>
+    </div>
+  )
+}
+
